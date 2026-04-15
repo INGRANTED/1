27b7496493a49e0f0ad9077be8d50581f4030966
NOTGRANDED <amin89310@gmail.com>
2026-04-15 08:49:49 +0600

    Create f.bat

---
+++ f.bat
@@ -0,0 +1,34 @@
+@echo off
+title Windows Optimizer - BLACK FOX STYLE
+color 0A
+
+echo =====================================
+echo        WINDOWS OPTIMIZER
+echo =====================================
+echo.
+
+echo [1/5] Cleaning TEMP files...
+del /s /f /q %temp%\* >nul 2>&1
+del /s /f /q C:\Windows\Temp\* >nul 2>&1
+
+echo [2/5] Flushing DNS...
+ipconfig /flushdns >nul
+
+echo [3/5] Resetting network cache...
+netsh winsock reset >nul
+
+echo [4/5] Releasing IP...
+ipconfig /release >nul
+ipconfig /renew >nul
+
+echo [5/5] Clearing Prefetch...
+del /s /f /q C:\Windows\Prefetch\* >nul 2>&1
+
+echo.
+echo =====================================
+echo        OPTIMIZATION DONE
+echo =====================================
+echo.
+
+timeout /t 2 >nul
+exit
